b21bc5532d95d2d6ea921762153c3c7f3e38228d
Nikita Begletskiy <steveefox50@icloud.com>
2020-05-29 08:31:20 +0300

    Initial Commit

---
+++ Login screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login screen
+//
+//  Created by Nikita on 5/29/20.
+//  Copyright © 2020 Nikita Begletskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
